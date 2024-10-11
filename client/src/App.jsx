cbc43b0fb5715a7ea608179aefc7b03990cc7ff3
San39090 <121235533+San39090@users.noreply.github.com>
2024-10-11 12:37:56 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import IndexPage from './pages/indexPages'
+import IndexPage from './pages/IndexPages'
 import LoginPage from './pages/LoginPage'
 import Layout from './Layout'
 import RegisterPage from './pages/RegisterPage'
