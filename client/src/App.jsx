0a64818a35fb7a2a80724d6cb97e0f5c6fb392a7
San39090 <121235533+San39090@users.noreply.github.com>
2024-10-11 12:30:22 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import PlacePage from './pages/PlacePage'
 import BookingPage from './pages/BookingPage'
 import BookingsPage from './pages/BookingsPage'
 
-axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = 'https://hotel-booking-app-czf3.onrender.com';
 axios.defaults.withCredentials=true;
 
 
